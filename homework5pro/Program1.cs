6e0a7db462ee8ec9f7477ba31d03f59b24672331
scrollme346 <133518123+scrollme346@users.noreply.github.com>
2024-01-18 21:45:54 +0200

    Program1.cs

--- homework5pro/Program1.cs
+++ homework5pro/Program1.cs
@@ -1,47 +1,25 @@
 using System;
 
-class Shop : IDisposable
+public class Shop : IDisposable
 {
+    private bool disposed = false;
+
     public string Name { get; set; }
     public string Address { get; set; }
-    public Shop.ShopType Type { get; set; } // Исправлено здесь
-
-    public enum ShopType
-    {
-        Grocery,
-        Household,
-        Clothing,
-        Footwear
-    }
+    public ShopType Type { get; set; }
 
     public Shop(string name, string address, ShopType type)
     {
         Name = name;
         Address = address;
         Type = type;
-        Console.WriteLine($"Создан новый магазин типа {Type}: {Name}, расположен по адресу {Address}");
     }
 
     public void DisplayInfo()
     {
-        Console.WriteLine($"Название магазина: {Name}");
-        Console.WriteLine($"Адрес: {Address}");
+        Console.WriteLine($"Назва: {Name}");
+        Console.WriteLine($"Адреса: {Address}");
         Console.WriteLine($"Тип: {Type}");
-    }
-
-    private bool disposed = false;
-
-    protected virtual void Dispose(bool disposing)
-    {
-        if (!disposed)
-        {
-            if (disposing)
-            {
-                Console.WriteLine($"Освобождение ресурсов для магазина типа {Type}: {Name}");
-            }
-
-            disposed = true;
-        }
     }
 
     public void Dispose()
@@ -50,22 +28,45 @@ class Shop : IDisposable
         GC.SuppressFinalize(this);
     }
 
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposed)
+        {
+            if (disposing)
+            {
+                // Викликати методи для вивільнення управляючих ресурсів
+                Console.WriteLine($"Магазин {Name} закритий.");
+            }
+
+            disposed = true;
+        }
+    }
+
     ~Shop()
     {
         Dispose(false);
     }
 }
 
+public enum ShopType
+{
+    Grocery,
+    Household,
+    Clothing,
+    Footwear
+}
+
 class Program
 {
     static void Main()
     {
-        using (var groceryShop = new Shop("Супермаркет", "123 Главная улица", Shop.ShopType.Grocery))
+        using (var groceryShop = new Shop("Продуктовий магазин", "вул. Продовольча, 123", ShopType.Grocery))
         {
             groceryShop.DisplayInfo();
-        }
 
-        
+            
+        } 
+
+        Console.ReadLine();
     }
 }
-
